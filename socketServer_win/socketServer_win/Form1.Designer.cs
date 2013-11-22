232f5a76a8bc8d9c963535946d7e9aa7ba656763
garrisonTony <garrison.z90@gmail.com>
2013-11-22 17:06:11 +0800

    v0.01

---
+++ socketServer_win/socketServer_win/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace socketServer_win {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.tb_history = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_msg = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tb_port = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checked_lb_client = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // tb_history
+            // 
+            this.tb_history.Location = new System.Drawing.Point(20, 58);
+            this.tb_history.Name = "tb_history";
+            this.tb_history.Size = new System.Drawing.Size(445, 246);
+            this.tb_history.TabIndex = 0;
+            this.tb_history.Text = "";
+            this.tb_history.TextChanged += new System.EventHandler(this.tb_history_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "历史记录";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_msg
+            // 
+            this.tb_msg.Location = new System.Drawing.Point(20, 378);
+            this.tb_msg.Name = "tb_msg";
+            this.tb_msg.Size = new System.Drawing.Size(443, 89);
+            this.tb_msg.TabIndex = 4;
+            this.tb_msg.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(498, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "客户端列表";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 26);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "启动服务";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tb_port
+            // 
+            this.tb_port.Location = new System.Drawing.Point(304, 22);
+            this.tb_port.Name = "tb_port";
+            this.tb_port.ReadOnly = true;
+            this.tb_port.Size = new System.Drawing.Size(63, 21);
+            this.tb_port.TabIndex = 7;
+            this.tb_port.Text = "8889";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "监听端口";
+            // 
+            // checked_lb_client
+            // 
+            this.checked_lb_client.FormattingEnabled = true;
+            this.checked_lb_client.Location = new System.Drawing.Point(500, 58);
+            this.checked_lb_client.Name = "checked_lb_client";
+            this.checked_lb_client.Size = new System.Drawing.Size(169, 308);
+            this.checked_lb_client.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 489);
+            this.Controls.Add(this.checked_lb_client);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_port);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_msg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_history);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox tb_history;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox tb_msg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tb_port;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checked_lb_client;
+    }
+}
+
---
+++ socketServer_win/socketServer_win/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+namespace socketServer_win {
+    public partial class Form1 : Form {
+
+        private string ipStr = "127.0.0.1";
+        private Socket serverS;
+        private int maxCliCount = 10;
+        private int byteLength = 1000;
+        private List<Socket> clientList = new List<Socket>();
+        private List<Socket> cliList_live;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.startServer();
+        }
+
+        public void startServer() {
+            int port = this.getPort();
+            this.startServer(port);
+        }
+
+        /**
+         * 启动服务
+         */
+        public void startServer(int port) {
+            IPAddress ip = IPAddress.Parse(ipStr);
+            IPEndPoint ipPoint = new IPEndPoint(ip, port);
+            if (serverS != null) {
+                String serverAddr = serverS.LocalEndPoint.ToString();
+                tb_history.AppendText("已启动监听 " + serverAddr + "\n");
+                return;
+            }
+
+            serverS = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverS.Bind(ipPoint);
+            serverS.Listen(maxCliCount);
+            tb_history.AppendText("启动监听" + serverS.LocalEndPoint.ToString() + "\n");
+
+            Thread newThread = new Thread(listenClientConn);
+            newThread.IsBackground = true;
+            newThread.Start();
+        }
+
+        /**
+         * 监听客户端
+         */
+        public void listenClientConn() {
+            while (true) {
+                Socket aSocket = serverS.Accept();
+
+                clientList.Add(aSocket);
+                showCliList();
+                appendToHistory("连接来自" + aSocket.RemoteEndPoint.ToString() + "\n");
+
+                Byte[] msgByte = Encoding.UTF8.GetBytes("这里是服务端");
+                aSocket.Send(msgByte);
+
+                Thread newThread = new Thread(RecieveMsg);
+                newThread.IsBackground = true;
+                newThread.Start(aSocket);
+            }
+        }
+
+        /**
+         * 接受信息
+         */
+        public void RecieveMsg(Object obj) {
+            Socket aSocket = (Socket)obj;
+            while (true) {
+                try {
+                    Byte[] res = new Byte[byteLength];
+                    int length = aSocket.Receive(res);
+                    String resString = Encoding.UTF8.GetString(res, 0, length);
+                    appendToHistory("来自" + aSocket.RemoteEndPoint.ToString() + "\n" + resString + "\n");
+                }
+                catch (Exception ex) {
+                    appendToHistory("异常：" + ex.Message + "\n");
+                    aSocket.Shutdown(SocketShutdown.Both);
+                    aSocket.Close();
+                    this.showCliList();
+                    break;
+                }
+            }
+        }
+
+        /**
+         * 获得端口号
+         */
+        public int getPort() {
+            String portStr = tb_port.Text;
+            int port = int.Parse(portStr);
+
+            return port;
+        }
+
+        private void tb_history_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private delegate void InvokeCallback(String msg);
+        /**
+         * 给历史记录增加文字
+         */
+        public void appendToHistory(String msg) {
+            if (tb_history.InvokeRequired) {
+                InvokeCallback callback = new InvokeCallback(appendToHistory);
+                tb_history.Invoke(callback, msg);
+            }
+            else {
+                if (tb_history == null) {
+                    return;
+                }
+                tb_history.AppendText(msg);
+            }
+        }
+
+        /**
+         * 增加客户端列表 ListBox
+         */
+        public void addToClientList_lb(String msg) {
+            if (checked_lb_client.InvokeRequired) {
+                InvokeCallback callback = new InvokeCallback(addToClientList_lb);
+                checked_lb_client.Invoke(callback, msg);
+            }
+            else {
+                checked_lb_client.Items.Add(msg);
+            }
+        }
+
+        /**
+         * 清空客户端列表 ListBox
+         */
+        private delegate void listInvokeCallback();
+        public void clearClientList_lb() {
+            if (checked_lb_client.InvokeRequired) {
+                listInvokeCallback callback = new listInvokeCallback(clearClientList_lb);
+                checked_lb_client.Invoke(callback);
+            }
+            else {
+                checked_lb_client.Items.Clear();
+            }
+        }
+
+
+
+
+        /**
+         * 显示连接状态的客户端
+         */
+        public void showCliList() {
+            clearClientList_lb();
+            cliList_live = this.getConnctedCli();
+
+            for (int i = 0; i < cliList_live.Count; i++) {
+                String cliAddress = cliList_live[i].RemoteEndPoint.ToString();
+                String cliInfo = i + ". " + cliAddress;
+                addToClientList_lb(cliInfo);
+            }
+        }
+
+        /**
+         * 获取连接状态中的客户端
+         */
+        public List<Socket> getConnctedCli() {
+            List<Socket> cliList = new List<Socket>();
+            foreach (Socket so in clientList) {
+                if (so.Connected && so.Poll(-1, SelectMode.SelectWrite)) {
+                    cliList.Add(so);
+                }
+            }
+
+            return cliList;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            String msg = getMsgContent();
+            List<Socket> sendList = this.getSendTarget();
+            if (sendList.Count == 0) {
+                appendToHistory("请选择发送对象\n");
+                return;
+            }
+            this.sendMsg(msg, sendList);
+
+            //显示发送信息
+            tb_history.AppendText("服务端 - 发往");
+            foreach (Socket so in sendList) {
+                tb_history.AppendText(so.RemoteEndPoint.ToString() + ", ");
+            }
+            tb_history.AppendText("\n");
+            tb_history.AppendText(msg + "\n");
+            tb_msg.Clear();
+        }
+
+        /**
+         * 发送消息
+         */
+        public void sendMsg(String msgStr, List<Socket> sendList) {
+            foreach (Socket so in sendList) { 
+                Byte[] msgByte = Encoding.UTF8.GetBytes(msgStr);
+                so.Send(msgByte);
+            }
+        }
+
+        /**
+         * 获得发送对象
+         */
+        public List<Socket> getSendTarget() {
+            List<Socket> sendList = new List<Socket>();
+            for (int i = 0; i < checked_lb_client.Items.Count; i++) {
+                if (checked_lb_client.GetItemChecked(i) == false) {
+                    continue;
+                }
+                String clientInfo = checked_lb_client.GetItemText(checked_lb_client.Items[i]);
+                String[] info = clientInfo.Split('.');
+                int index = int.Parse(info[0]);
+                sendList.Add(cliList_live[index]);
+            }
+
+            return sendList;
+        }
+
+        /**
+         * 获取发送内容
+         */
+        public String getMsgContent() {
+            String msg = tb_msg.Text;
+
+            return msg;
+        }
+    }
+}
